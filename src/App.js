c2688222f15b8cbe0e71ea94c4618e198867b926
rimi <a.fatimaarimi@gmail.com>
2023-08-04 10:49:10 -0400

    added api key and fetch

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ import MovieDisplay from "./components/MovieDisplay.js";
 import Form from "./components/Form.js";
 
 export default function App() {
+  //variable with your apiKey
+  const apiKey = "98e3fb1f";
+
+  //State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
   // USE OUR COMPONENTS IN APPs RETURNED JSX
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
-    // Write JSX here -- JSX is what will return and display on our screen like HTML
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
